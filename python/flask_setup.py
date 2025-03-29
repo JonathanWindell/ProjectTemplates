6a468e39f3e1a21a6085932ad13edba6f9b3a5ed
Jonathan Windell <windelljonathan04@gmail.com>
2025-03-29 13:02:00 +0100

    Commit fixat Import för database connection

--- python/flask_setup.py
+++ python/flask_setup.py
@@ -1,4 +1,9 @@
 #Flask setup with database connection
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import psycopg2
+from datetime import datetime
+
 app = Flask(__name__)
 CORS(app)  # Allows cross-origin requests
 
